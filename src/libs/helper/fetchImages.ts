179eb3517240dd3b57f31c73fe81349a660d841b
superfeddy <fedf0415@gmail.com>
2023-09-21 22:58:21 -0400

    - Add customHook to fetch the images - Add helper function to integrate APIs

--- src/libs/helper/fetchImages.ts
+++ src/libs/helper/fetchImages.ts
@@ -1,1 +1,27 @@
-export const fetchImages = () => {};
+import { IFilterOption } from "../types/FilterOptions.type";
+import { IImageInfo } from "../types/ImageInfo.types";
+
+interface IResponseResult {
+  urls: IImageInfo;
+  [key: string]: any;
+}
+
+export const fetchImages = async ({
+  keyword,
+  sortMethod,
+  colorFilter,
+}: IFilterOption) => {
+  const url = `${process.env.API_BASE_URL}/search/photos?client_id=${
+    process.env.UNSPLASH_ACCESS_KEY
+  }&order_by=${sortMethod}${keyword ?? `&query=${keyword}`}${
+    colorFilter && `&color=${colorFilter}`
+  }`;
+  try {
+    const jsonData = await fetch(url);
+    const { results, total_pages, total } = await jsonData.json();
+    const images = results.map(({ urls }: IResponseResult) => urls);
+    return { images, total, total_pages };
+  } catch (error) {
+    throw error;
+  }
+};
--- src/libs/hooks/useImages.ts
+++ src/libs/hooks/useImages.ts
@@ -1,1 +1,53 @@
-export const useImages = () => {};
+import { IFilterOption } from "../types/FilterOptions.type";
+import { useState, useEffect } from "react";
+import { IImageInfo } from "../types/ImageInfo.types";
+import { fetchImages } from "../helper/fetchImages";
+
+interface IData {
+  images: IImageInfo[];
+  total: number;
+  total_pages: number;
+}
+
+export const useImages = ({
+  keyword,
+  sortMethod,
+  colorFilter,
+}: IFilterOption) => {
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+  const [imageData, setImageData] = useState<IData | undefined>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { images, total, total_pages } = await fetchImages({
+          keyword,
+          sortMethod,
+          colorFilter,
+        });
+        setLoading(false);
+        setError(false);
+        setImageData({
+          images,
+          total,
+          total_pages,
+        });
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+        setImageData(undefined);
+      }
+    };
+
+    //  Init the status
+    setLoading(true);
+    setError(false);
+    setImageData(undefined);
+
+    //  Fetch data
+    fetchData();
+  }, [keyword, sortMethod, colorFilter]);
+
+  return [isLoading, isError, imageData];
+};
